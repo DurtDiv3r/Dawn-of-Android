a6705e44cd5593e1f0c1343d91ceb592197ae361
DurtDiv3r <durtdev@gmail.com>
2023-06-16 17:17:19 +1200

    2-Setup-Initial-Dependency-Injection: Add DI annotations etc. Initial NetworkModule commit.

--- app/src/main/java/com/islaharper/dawnofandroid/util/Constants.kt
+++ app/src/main/java/com/islaharper/dawnofandroid/util/Constants.kt
@@ -1,6 +1,11 @@
 package com.islaharper.dawnofandroid.util
 
 object Constants {
+
+    // BACKEND
+    // Using Localhost for testing
+    const val BASE_URL = "http://127.0.0.1:8080"
+
     // Room DB
     const val FLAVOUR_DB = "flavour_db"
     const val FLAVOUR_DB_TABLE = "flavour_table"
