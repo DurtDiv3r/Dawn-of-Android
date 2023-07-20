59a001e936d9676229fd9e1987d48b0bd260e885
DurtDiv3r <durtdev@gmail.com>
2023-07-21 11:51:13 +1200

    13-Setup-HomeScreen: Create UseCase to retrieve flavours.

---
+++ app/src/main/java/com/islaharper/dawnofandroid/domain/useCases/getAllFlavours/GetAllFlavoursUseCase.kt
@@ -0,0 +1,9 @@
+package com.islaharper.dawnofandroid.domain.useCases.getAllFlavours
+
+import androidx.paging.PagingData
+import com.islaharper.dawnofandroid.domain.model.Flavour
+import kotlinx.coroutines.flow.Flow
+
+interface GetAllFlavoursUseCase {
+    suspend operator fun invoke(): Flow<PagingData<Flavour>>
+}
---
+++ app/src/main/java/com/islaharper/dawnofandroid/domain/useCases/getAllFlavours/GetAllFlavoursUseCaseImpl.kt
@@ -0,0 +1,14 @@
+package com.islaharper.dawnofandroid.domain.useCases.getAllFlavours
+
+import androidx.paging.PagingData
+import com.islaharper.dawnofandroid.data.repository.Repository
+import com.islaharper.dawnofandroid.domain.model.Flavour
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class GetAllFlavoursUseCaseImpl @Inject constructor(private val repository: Repository) :
+    GetAllFlavoursUseCase {
+    override suspend fun invoke(): Flow<PagingData<Flavour>> {
+        return repository.getAllFlavours()
+    }
+}
