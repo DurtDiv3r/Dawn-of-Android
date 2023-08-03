040f70c36d84e6dcbc5fa72cb2cd6de6b4eb56ae
DurtDiv3r <durtdev@gmail.com>
2023-08-04 10:31:21 +1200

    9-Setup-Login-Screen-Components: Refactor, remove typeAlias

--- app/src/main/java/com/islaharper/dawnofandroid/domain/useCases/signInClient/SignInClientUseCaseImpl.kt
+++ app/src/main/java/com/islaharper/dawnofandroid/domain/useCases/signInClient/SignInClientUseCaseImpl.kt
@@ -1,12 +1,13 @@
 package com.islaharper.dawnofandroid.domain.useCases.signInClient
 
-import com.islaharper.dawnofandroid.data.repository.OneTapSignInResponse
+import com.google.android.gms.auth.api.identity.BeginSignInResult
 import com.islaharper.dawnofandroid.domain.repository.AuthRepository
+import com.islaharper.dawnofandroid.util.Resource
 import javax.inject.Inject
 
 class SignInClientUseCaseImpl @Inject constructor(private val repository: AuthRepository) :
     SignInClientUseCase {
-    override suspend operator fun invoke(): OneTapSignInResponse {
+    override suspend operator fun invoke(): Resource<BeginSignInResult> {
         return repository.oneTapSignIn()
     }
 }
