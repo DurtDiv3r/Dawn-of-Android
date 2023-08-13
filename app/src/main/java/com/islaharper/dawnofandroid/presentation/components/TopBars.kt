c853eb866b216f9d3c1f986a43c987beea7b6550
Brian Saunders <durtdev@gmail.com>
2023-08-14 09:03:24 +1200

    11-Components: Add previews for SearchTopBar.

--- app/src/main/java/com/islaharper/dawnofandroid/presentation/components/TopBars.kt
+++ app/src/main/java/com/islaharper/dawnofandroid/presentation/components/TopBars.kt
@@ -1,6 +1,8 @@
 package com.islaharper.dawnofandroid.presentation.components
 
+import android.content.res.Configuration
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun SearchTopBar(
@@ -14,5 +16,24 @@ fun SearchTopBar(
         onTextChanged = onTextChanged,
         onSearchClicked = onSearchClicked,
         onCloseClicked = onCloseClicked
+    )
+}
+
+@Preview(name = "Light")
+@Preview(name = "Dark", uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+fun SearchTopBarPreview() {
+    SearchWidget(text = "", onTextChanged = {}, onSearchClicked = {}, onCloseClicked = {})
+}
+
+@Preview(name = "Light")
+@Preview(name = "Dark", uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+fun SearchTopBarTextPreview() {
+    SearchWidget(
+        text = "Searched Text",
+        onTextChanged = {},
+        onSearchClicked = {},
+        onCloseClicked = {}
     )
 }
