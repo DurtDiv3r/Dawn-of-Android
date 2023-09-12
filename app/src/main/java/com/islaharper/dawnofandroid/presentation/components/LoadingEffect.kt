a9c36a8b7740e5505ef92478d6a83c9bec28df7c
Brian Saunders <durtdev@gmail.com>
2023-09-13 11:42:03 +1200

    HomeScreen Components: Animated loading effect

---
+++ app/src/main/java/com/islaharper/dawnofandroid/presentation/components/LoadingEffect.kt
@@ -0,0 +1,127 @@
+package com.islaharper.dawnofandroid.presentation.components
+
+import android.content.res.Configuration
+import androidx.compose.animation.core.FastOutLinearInEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Surface
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.islaharper.dawnofandroid.R
+import com.islaharper.dawnofandroid.ui.theme.DawnOfAndroidTheme
+import com.islaharper.dawnofandroid.ui.theme.PADDING_LARGE
+import com.islaharper.dawnofandroid.ui.theme.PADDING_MEDIUM
+import com.islaharper.dawnofandroid.ui.theme.PADDING_SMALL
+import com.islaharper.dawnofandroid.ui.theme.PADDING_XSMALL
+
+@Composable
+fun LoadingEffect(modifier: Modifier = Modifier) {
+    LazyColumn(
+        modifier = modifier.background(MaterialTheme.colorScheme.surface),
+        contentPadding = PaddingValues(all = PADDING_SMALL),
+        verticalArrangement = Arrangement.spacedBy(PADDING_MEDIUM),
+    ) {
+        items(count = 2) {
+            AnimatedLoadingItem()
+        }
+    }
+}
+
+@Composable
+fun AnimatedLoadingItem(modifier: Modifier = Modifier) {
+    val transition = rememberInfiniteTransition(label = "")
+    val alphaAnim by transition.animateFloat(
+        initialValue = 1f,
+        targetValue = 0f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(
+                durationMillis = 650,
+                easing = FastOutLinearInEasing,
+            ),
+            repeatMode = RepeatMode.Reverse,
+        ),
+        label = stringResource(R.string.loading_animation_label),
+    )
+
+    Surface(
+        modifier = modifier
+            .fillMaxWidth()
+            .height(400.dp),
+        color = MaterialTheme.colorScheme.onSurfaceVariant,
+        shape = RoundedCornerShape(size = PADDING_LARGE),
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(PADDING_MEDIUM),
+            verticalArrangement = Arrangement.Bottom,
+        ) {
+            Surface(
+                modifier = Modifier
+                    .fillMaxWidth(0.5f)
+                    .height(30.dp)
+                    .alpha(alpha = alphaAnim),
+                color = MaterialTheme.colorScheme.surface,
+                shape = RoundedCornerShape(PADDING_SMALL),
+
+            ) {}
+            Spacer(modifier = Modifier.padding(PADDING_MEDIUM))
+            repeat(3) {
+                Surface(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(15.dp)
+                        .alpha(alpha = alphaAnim),
+                    color = MaterialTheme.colorScheme.surface,
+                    shape = RoundedCornerShape(PADDING_SMALL),
+                ) {}
+                Spacer(modifier = Modifier.padding(PADDING_SMALL))
+            }
+            Row(modifier = Modifier.fillMaxWidth()) {
+                repeat(5) {
+                    Surface(
+                        modifier = Modifier
+                            .size(20.dp)
+                            .alpha(alpha = alphaAnim),
+                        color = MaterialTheme.colorScheme.surface,
+                        shape = RoundedCornerShape(PADDING_SMALL),
+                    ) {}
+                    Spacer(modifier = Modifier.padding(PADDING_XSMALL))
+                }
+            }
+        }
+    }
+}
+
+@Preview(name = "Light", showBackground = true)
+@Preview(name = "Dark", showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+fun LoadingItemPreview() {
+    DawnOfAndroidTheme {
+        Surface {
+            LoadingEffect()
+        }
+    }
+}
--- app/src/main/java/com/islaharper/dawnofandroid/ui/theme/Dimens.kt
+++ app/src/main/java/com/islaharper/dawnofandroid/ui/theme/Dimens.kt
@@ -2,16 +2,11 @@ package com.islaharper.dawnofandroid.ui.theme
 
 import androidx.compose.ui.unit.dp
 
+val PADDING_XSMALL = 3.dp
 val PADDING_SMALL = 6.dp
 val PADDING_MEDIUM = 16.dp
 val PADDING_LARGE = 20.dp
 val PADDING_XLARGE = 40.dp
-
-val FLAVOUR_ITEM_HEIGHT = 400.dp
-
-val NAME_PLACEHOLDER_HEIGHT = 30.dp
-val DESCRIPTION_PLACEHOLDER_HEIGHT = 15.dp
-val RATING_PLACEHOLDER_HEIGHT = 20.dp
 
 val TOP_BAR_HEIGHT = 56.dp
 val MIN_SHEET_HEIGHT = 140.dp
