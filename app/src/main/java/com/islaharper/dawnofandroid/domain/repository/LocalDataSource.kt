4f39b8929809a609af3c8f0da228b701ce95923f
Brian Saunders <durtdev@gmail.com>
2023-09-13 12:26:55 +1200

    RemoteKeys: Initial setup of remote keys for paging.

--- app/src/main/java/com/islaharper/dawnofandroid/domain/repository/LocalDataSource.kt
+++
@@ -1,7 +0,0 @@
-package com.islaharper.dawnofandroid.domain.repository
-
-import com.islaharper.dawnofandroid.domain.model.Flavour
-
-interface LocalDataSource {
-    suspend fun getSelectedFlavour(flavourId: Int): Flavour
-}
